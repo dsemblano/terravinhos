5f23a536193a235d117b98c65ff909429ca8c717
Daniel Semblano <dsemblano@gmail.com>
2026-04-30 02:22:12 -0300

    Wine page products more attributes, improvements and svgs

--- site/web/app/themes/btv/resources/views/woocommerce/single/specs.blade.php
+++ site/web/app/themes/btv/resources/views/woocommerce/single/specs.blade.php
@@ -4,7 +4,7 @@
     <div class="pt-4">
         <h3 class="font-semibold text-primary mb-2">Características</h3>
 
-        <ul class="text-gray-700 list-none grid grid-cols-2 text-xl">
+        <ul class="text-gray-700 list-none grid grid-cols-2 text-lg">
             @foreach ($product->get_attributes() as $attribute)
                 @php
                     // Obtém o slug (ex: pa_corpo)
@@ -16,8 +16,10 @@
                     $value = implode(', ', wc_get_product_terms($product->get_id(), $attr_slug, ['fields' => 'names']));
                 @endphp
 
-                <li class="li-{{ $class_name }}">
-                    <strong>{{ $name }}:</strong> {{ $value }}
+                <li class="li-{{ $class_name }} inline-flex">
+                    <span class="flex flex-col">
+                        <strong>{{ $name }}</strong> {{ $value }}
+                    </span>
                 </li>
             @endforeach
         </ul>
